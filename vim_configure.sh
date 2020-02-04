51c6e42a7e31c1ba76b362c07aee622846325416
azhong <zczwzw@163.com>
2020-02-05 05:32:56 +0800

    add vim_configure.sh

--- vim_configure.sh
+++ vim_configure.sh
@@ -1,1 +1,13 @@
 #!/bin/sh
+
+if [ ! -d ~/.vimrc ]; then
+    touch ~/.vimrc
+fi
+echo "set ts=4" > ~/.vimrc
+echo "set expandtab" >> ~/.vimrc
+echo "set autoindent" >> ~/.vimrc
+echo "syntax enable" >> ~/.vimrc
+echo "set nocompatible" >> ~/.vimrc
+echo "set backspace=indent,eol,start" >> ~/.vimrc
+
+echo "finish adding configure for vim "
